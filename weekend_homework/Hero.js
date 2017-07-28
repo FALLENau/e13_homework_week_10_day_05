f672f712e942048097c53a5d5be2859b96927d03
FALLENau <reece.c.jones@gmail.com>
2017-07-28 12:04:00 +0100

    hero

--- weekend_homework/Hero.js
+++ weekend_homework/Hero.js
@@ -1,1 +1,15 @@
-var Hero = Object.create()
+var Hero = function(name, health, favFood) {
+  this.name = name
+  this.health = health
+  this.favFood = favFood
+  this.heroTasks = []
+}
+
+Hero.prototype.heroCanTalk = function(){
+  return "Hello my name is " + this.name
+}
+
+// var heroTasks = [{task: "Save the day", difficulty: 7}, {"Kissed the girl", 10}, {"kick some badguy ass", 4}]
+var batman = new Hero('Batman', 100, 'His Greens')
+
+console.log(batman.heroCanTalk())
